60ffd47092d62a09e327619e6e012c4a63fb2e69
matheus.pita <matheus.pita@SPON3245.sp01.local>
2017-05-30 14:26:22 -0300

    atualização dao client

--- BancoToisV2/src/DAO/CtrlCliente.java
+++ BancoToisV2/src/DAO/CtrlCliente.java
@@ -3,7 +3,9 @@ package DAO;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
+import tois.model.Conta;
 import tois.model.Correntista;
 
 public class CtrlCliente {
@@ -16,6 +18,31 @@ public class CtrlCliente {
 		em.getTransaction().commit();
 		em.clear();
 		factory.close();
+	}
+	
+	public static void delCliente(Conta conta, Correntista correntista, String senha, String cpf){
+		String log;
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("BANCO");
+		EntityManager em = factory.createEntityManager();
+		
+		if(cpf.equals(correntista.getCPF())&& senha.equals(conta.getSenha())){
+				try{
+					Query query = em.createQuery
+							("DELETE FROM Conta WHERE"
+									+ "numConta =: num");
+					
+					Query query2 = em.createQuery
+							("DELETE FROM Usuarios WHERE"
+									+ "CPF =: cpf");
+					
+					query.setParameter("numConta", conta.getNumConta());
+					query2.setParameter("cpf", correntista.getCPF());
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+		}else{
+			log = "Valores inválidos";
+		}
 	}
 //	
 //	private boolean readCliente(){
